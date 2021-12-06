8109a01dd4fbbc1c861961494215244cd1e96904
Ellie Chapman <ellie@amadido.co.uk>
2021-12-06 08:35:39 +0000

    fixed day 5 part 1, added part 2

--- day05_part1.py
+++ day05_part1.py
@@ -2,12 +2,9 @@ f = open('day05_input.txt', 'r')
 a = f.readlines()
 
 for i in range(0, len(a)):
-    a[i] = a[i].replace("\n", "").replace(",", " ").replace(" -> ", " ").split(" ")
-
-# print(a)
+    a[i] = a[i].replace("\n", "").replace(",", " ").replace(" -> ", " ").lstrip().rstrip().split(" ")
 
 found = {}
-
 def update_dict(s):
     if s in found:
         found[s] += 1
@@ -15,25 +12,48 @@ def update_dict(s):
         found[s] = 1
 
 def xsame(coord):
-    if coord[1] < coord[3]:
+    if int(coord[1]) < int(coord[3]):
         for y in range(int(coord[1]), int(coord[3])+1):
             update_dict(coord[0]+","+str(y))
-    elif coord[1] > coord[3]:
+    elif int(coord[1]) > int(coord[3]):
         for y in range(int(coord[3]), int(coord[1])+1):
             update_dict(coord[0]+","+str(y))
     else:
         print("uh oh")
 
-
 def ysame(coord):
-    if coord[0] < coord[2]:
+    if int(coord[0]) < int(coord[2]):
         for x in range(int(coord[0]), int(coord[2])+1):
             update_dict(str(x)+","+coord[1])
-    elif coord[0] > coord[2]:
+    elif int(coord[0]) > int(coord[2]):
         for x in range(int(coord[2]), int(coord[0])+1):
             update_dict(str(x)+","+coord[1])
     else:
         print("uh oh")
+
+def diag(coord):
+    x1 = int(coord[0])
+    x2 = int(coord[2])
+    y1 = int(coord[1])
+    y2 = int(coord[3])
+    if x1 < x2:
+        if y1 < y2:
+            for i in range(x1, x2+1):
+                y = y1 + (i-x1)
+                update_dict(str(i)+","+str(y))
+        else:
+            for i in range(x1, x2+1):
+                y = y1 - (i-x1)
+                update_dict(str(i)+","+str(y))
+    else:
+        if y1 < y2:
+            for i in range(x1, x2-1, -1):
+                y = y1 + (x1-i)
+                update_dict(str(i)+","+str(y))
+        else:
+            for i in range(x1, x2-1, -1):
+                y = y1 - (x1-i)
+                update_dict(str(i)+","+str(y))
 
 
 for coord in a:
@@ -44,12 +64,12 @@ for coord in a:
     elif coord[1] == coord[3]:
         ysame(coord)
     else:
-        pass
+        diag(coord)
+        # pass
         # ignore in part 1
 
 danger = 0
 for key in found:
-    # print(key, found[key])
     if found[key] >= 2:
         danger += 1
 print(danger)
