38e8a9b844f1094f371aad3167b040d190d84835
Ellie Chapman <ellie@amadido.co.uk>
2021-12-08 20:46:50 +0000

    parsing inout into ints, using tuples as keys

--- day05_part1.py
+++ day05_part1.py
@@ -2,7 +2,9 @@ f = open('day05_input.txt', 'r')
 a = f.readlines()
 
 for i in range(0, len(a)):
-    a[i] = a[i].replace("\n", "").replace(",", " ").replace(" -> ", " ").lstrip().rstrip().split(" ")
+    a[i] = a[i].replace("\n", "").replace(",", " ").replace(" -> ", " ").split(" ")
+    for j in range(0, len(a[i])):
+        a[i][j] = int(a[i][j])
 
 found = {}
 def update_dict(s):
@@ -12,48 +14,48 @@ def update_dict(s):
         found[s] = 1
 
 def xsame(coord):
-    if int(coord[1]) < int(coord[3]):
-        for y in range(int(coord[1]), int(coord[3])+1):
-            update_dict(coord[0]+","+str(y))
-    elif int(coord[1]) > int(coord[3]):
-        for y in range(int(coord[3]), int(coord[1])+1):
-            update_dict(coord[0]+","+str(y))
+    if coord[1] < coord[3]:
+        for y in range(coord[1], coord[3]+1):
+            update_dict((coord[0], y))
+    elif coord[1] > coord[3]:
+        for y in range(coord[3], coord[1]+1):
+            update_dict((coord[0], y))
     else:
         print("uh oh")
 
 def ysame(coord):
-    if int(coord[0]) < int(coord[2]):
-        for x in range(int(coord[0]), int(coord[2])+1):
-            update_dict(str(x)+","+coord[1])
-    elif int(coord[0]) > int(coord[2]):
-        for x in range(int(coord[2]), int(coord[0])+1):
-            update_dict(str(x)+","+coord[1])
+    if coord[0] < coord[2]:
+        for x in range(coord[0], coord[2]+1):
+            update_dict((x, coord[1]))
+    elif coord[0] > coord[2]:
+        for x in range(coord[2], coord[0]+1):
+            update_dict((x, coord[1]))
     else:
         print("uh oh")
 
 def diag(coord):
-    x1 = int(coord[0])
-    x2 = int(coord[2])
-    y1 = int(coord[1])
-    y2 = int(coord[3])
+    x1 = coord[0]
+    x2 = coord[2]
+    y1 = coord[1]
+    y2 = coord[3]
     if x1 < x2:
         if y1 < y2:
             for i in range(x1, x2+1):
                 y = y1 + (i-x1)
-                update_dict(str(i)+","+str(y))
+                update_dict((i, y))
         else:
             for i in range(x1, x2+1):
                 y = y1 - (i-x1)
-                update_dict(str(i)+","+str(y))
+                update_dict((i, y))
     else:
         if y1 < y2:
             for i in range(x1, x2-1, -1):
                 y = y1 + (x1-i)
-                update_dict(str(i)+","+str(y))
+                update_dict((i, y))
         else:
             for i in range(x1, x2-1, -1):
                 y = y1 - (x1-i)
-                update_dict(str(i)+","+str(y))
+                update_dict((i, y))
 
 
 for coord in a:
